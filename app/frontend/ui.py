df8007681d31ee3560329159dfdcd9b9a31f657a
ZR <sqlpractice19@gmail.com>
2026-02-12 12:24:05 -0500

    ui modified

--- app/frontend/ui.py
+++ app/frontend/ui.py
@@ -8,7 +8,7 @@ from app.common.custom_exception import CustomException
 logger = get_logger(__name__)
 
 st.set_page_config(page_title="Multi AI Agent" , layout="centered")
-st.title("Multi AI Agent using Groq and Tavily New")
+st.title("Multi AI Agent using Groq and Tavily")
 
 system_prompt = st.text_area("Define your AI Agent: " , height=70)
 selected_model = st.selectbox("Select your AI model: ", settings.ALLOWED_MODEL_NAMES)
